fccd0dfac0ea3896f1a3472416b5c0e52d479555
maruf-ahmed <marufahamed1110@gmail.com>
2024-11-25 17:38:18 +0600

    update

--- src/app/modules/bicycle/bicycle.model.ts
+++ src/app/modules/bicycle/bicycle.model.ts
@@ -1,19 +1,24 @@
 import { model, Schema } from "mongoose";
 import { BiCycleInterface } from "./bicycle.interface";
 
-const bicycleSchema = new Schema<BiCycleInterface>({
-  name: { type: String, required: true },
-  brand: { type: String, required: true },
-  price: { type: Number, required: true },
-  type: {
-    type: String,
-    required: true,
-    enum: ["Mountain", "Road", "Hybrid", "BMX", "Electric"],
+const bicycleSchema = new Schema<BiCycleInterface>(
+  {
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    price: { type: Number, required: true },
+    type: {
+      type: String,
+      required: true,
+      enum: ["Mountain", "Road", "Hybrid", "BMX", "Electric"],
+    },
+    description: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    inStock: { type: Boolean, required: true },
   },
-  description: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  inStock: { type: Boolean, required: true },
-});
+  {
+    timestamps: true,
+  }
+);
 
 //  Create a Model-------------->
-export const BicycleModel = model<BiCycleInterface>('Bicycle', bicycleSchema);
+export const BicycleModel = model<BiCycleInterface>("Bicycle", bicycleSchema);
